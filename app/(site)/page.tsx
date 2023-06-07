3bf825c9a6fe030d915bed0b03461ca80d3e8e8c
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-07 23:58:02 +0200

    setup sidebar components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>main content</div>;
+  return <div>main</div>;
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,9 @@
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -20,6 +23,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         href: "/",
       },
       {
+        icon: BiSearch,
         label: "Search",
         active: pathname === "/search",
         href: "/search",
@@ -27,7 +31,21 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     ],
     [pathname]
   );
-  return <div>{children}</div>;
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Song library</Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
 };
 
 export default Sidebar;
