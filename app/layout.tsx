b198b753fc536d26edfeced666b43da4080a8ad2
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-11 20:22:20 +0200

    implement toaster to display errors and success

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Figtree } from "next/font/google";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProivder from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProivder />
