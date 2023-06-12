8af461350a0bf3b9f923b01ab92c0253e17a92ba
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-12 21:19:15 +0200

    open auth clicking on library

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,9 +3,16 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
 const Library = () => {
+  const authModal = useAuthModal();
+  const { user } = useUser();
   const onClick = () => {
-    //handle upload (later)
+    if (!user) {
+      return authModal.onOpen();
+    }
   };
   return (
     <div className="flex flex-col">
