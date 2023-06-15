a01c3a47e82870c89b2cc61d8ff1e783089e7229
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-15 11:42:36 +0200

    create page content, song item components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,7 @@ import getSongs from "@/actions/getSongs";
 
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 
@@ -21,11 +22,7 @@ export default async function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
         </div>
-        <div>
-          {songs.map((song) => (
-            <div>{song.title}</div>
-          ))}
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
