5acd254b137fa1c2952b2967b88eb2db509a05ff
Michał Bryk <galeriakoloru90210@gmail.com>
2023-06-19 14:03:00 +0200

    player content boilerplate

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,6 +4,8 @@ import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 
+import PlayerContent from "./PlayerContent";
+
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
@@ -14,7 +16,11 @@ const Player = () => {
     return null;
   }
 
-  return <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">player</div>;
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent song={song} songUrl={songUrl} key={songUrl} />
+    </div>
+  );
 };
 
 export default Player;
